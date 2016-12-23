1e0777077420d54067bc28925c762d46748cfdd9
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-12-22 17:00:15 -0800

    Scaffold core class

---
+++ source/ScalableVectorGraphicsEditing.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PressBits\MediaLibrary;
+
+/**
+ * Scalable Vector Graphics Editing.
+ *
+ * When editing is enabled, hook into the WordPress lifecycle.
+ *
+ * @since 0.1.0
+ */
+class ScalableVectorGraphicsEditing
+{
+    protected static $enabled = false;
+
+    /**
+     * Enable by hooking into the WordPress lifecycle.
+     *
+     * @since 0.1.0
+     */
+    public static function enable()
+    {
+        if (static::$enabled) {
+            return;
+        }
+        add_filter('wp_image_editors', [__CLASS__, 'add_editor']);
+        static::$enabled = true;
+    }
+
+    /**
+     * Add the SVG editor to available image editors.
+     *
+     * @since 0.1.0
+     * @param array $editors
+     * @return array
+     */
+    public static function add_editor( $editors ) {
+        return $editors;
+    }
+}
